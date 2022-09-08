25987298f4e4fa120dc8c7ddc16f79aa9bac9eff
nccvector <fzn1020@gmail.com>
2022-09-08 20:39:42 +0900

    new qt theme

--- main.py
+++ main.py
@@ -1,44 +1,57 @@
 import sys
-from PySide2 import QtCore, QtWidgets, QtGui
-from qt_material import apply_stylesheet
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+
+import qdarktheme
 import PyVenom
 
 import time
 
 
-class Viewport3D(QtWidgets.QDockWidget):
+class VenomDockWidget(QDockWidget):
+    def __init__(self, title="Dock Widget", parent=None):
+        super().__init__(title, parent)
+
+        self.setMinimumHeight(200)
+        self.setMinimumWidth(200)
+
+        # self.setStyleSheet("QDockWidget"
+        #                    "{"
+        #                    "background : rgb(50, 50, 90);"
+        #                    "}"
+        #                    "QDockWidget::title"
+        #                    "{"
+        #                    "background : rgb(50, 50, 90);"
+        #                    "}"
+        #                    "QDockWidget QPushButton"
+        #                    "{"
+        #                    "border : 2px solid black;"
+        #                    "background : darkgreen;"
+        #                    "}"
+        #                    )
+
+
+class Viewport3D(QLabel):
     def __init__(self, graphicsApp, title="Viewport", parent=None):
         super().__init__(title, parent)
 
-        self.titlebarHeight = self.style().pixelMetric(
-            QtWidgets.QStyle.PM_TitleBarHeight) * 1.2
-        self.drawRegion = QtWidgets.QLabel(self)
+        self.titlebarHeight = 0
+        # self.titlebarHeight = self.style().pixelMetric(
+        #     QStyle.PM_TitleBarHeight) * 1.2
+
+        self.drawRegion = QLabel(self)
 
         # Ogre3D app
         self.graphicsApp = graphicsApp
         self.graphicsApp.init(self.drawRegion.winId())
 
-        self.timer = QtCore.QTimer(self)
+        self.timer = QTimer(self)
         self.timer.timeout.connect(self.renderLoop)
 
         self.currentTime = time.time()
 
-        self.setStyleSheet("QDockWidget"
-                           "{"
-                           "background : rgb(50, 50, 90);"
-                           "}"
-                           "QDockWidget::title"
-                           "{"
-                           "background : rgb(50, 50, 90);"
-                           "}"
-                           "QDockWidget QPushButton"
-                           "{"
-                           "border : 2px solid black;"
-                           "background : darkgreen;"
-                           "}"
-                           )
-
-    def resizeEvent(self, event: QtGui.QResizeEvent):
+    def resizeEvent(self, event: QResizeEvent):
         self.graphicsApp.resize(event.size().width(), event.size().height())
         return super().resizeEvent(event)
 
@@ -57,17 +70,24 @@ class Viewport3D(QtWidgets.QDockWidget):
         print("FPS: ", 1/deltaTime)
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Venom Graphics")
         self.resize(1280, 720)
 
-        # self.centralWidget = QtWidgets.QCalendarWidget()
+        # Setting theme and attributes
+        self.setStyleSheet(qdarktheme.load_stylesheet())
+
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+
+        # Central widget is viewport now...
+        # self.centralWidget = QCalendarWidget()
         # self.setCentralWidget(self.centralWidget)
 
-        layout = QtWidgets.QHBoxLayout()
+        layout = QHBoxLayout()
         bar = self.menuBar()
         file = bar.addMenu('File')
         file.addAction('New')
@@ -79,19 +99,19 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Viewport3D
         self.viewport = Viewport3D(self.graphicsApp, parent=self)
-        self.viewport.setFloating(False)
-        self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.viewport)
-        # self.setCentralWidget(self.viewport)
+        # self.viewport.setFloating(False)
+        # self.addDockWidget(Qt.RightDockWidgetArea, self.viewport)
+        self.setCentralWidget(self.viewport)
 
         # Heirachy panel
-        self.heirarchyPanel = QtWidgets.QDockWidget("Heirarchy", parent=self)
+        self.heirarchyPanel = VenomDockWidget("Heirarchy", parent=self)
         self.heirarchyPanel.setFloating(False)
-        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.heirarchyPanel)
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.heirarchyPanel)
 
         # Inspector panel
-        self.inspectorPanel = QtWidgets.QDockWidget("Inspector", parent=self)
+        self.inspectorPanel = VenomDockWidget("Inspector", parent=self)
         self.inspectorPanel.setFloating(False)
-        self.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.inspectorPanel)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.inspectorPanel)
 
         self.setLayout(layout)
 
@@ -100,11 +120,11 @@ class MainWindow(QtWidgets.QMainWindow):
 
 if __name__ == '__main__':
     # Staring Qt app
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     mainWin = MainWindow()
 
-    # setup stylesheet
-    apply_stylesheet(app, theme='dark_red.xml')
+    # Anti-aliasing
+    app.setAttribute(Qt.ApplicationAttribute.AA_UseHighDpiPixmaps)
 
     mainWin.show()
     res = app.exec_()
